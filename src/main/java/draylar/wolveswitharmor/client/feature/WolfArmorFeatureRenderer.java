9a5481dd5ca5cf606879332feed8bf5d6f011a31
Draylar <samuelhimes@gmail.com>
2020-08-17 17:52:33 -0500

    rename golden wolf armor texture, update to 1.16, add maybe data include

--- src/main/java/draylar/wolveswitharmor/client/feature/WolfArmorFeatureRenderer.java
+++ src/main/java/draylar/wolveswitharmor/client/feature/WolfArmorFeatureRenderer.java
@@ -53,7 +53,7 @@ public class WolfArmorFeatureRenderer extends FeatureRenderer<WolfEntity, WolfEn
                 s = 1.0F;
             }
 
-            VertexConsumer vertexConsumer = ItemRenderer.method_27952(vertexConsumerProvider, RenderLayer.getArmorCutoutNoCull(armorItem.getEntityTexture()), false, itemStack.hasEnchantments());
+            VertexConsumer vertexConsumer = ItemRenderer.getArmorVertexConsumer(vertexConsumerProvider, RenderLayer.getArmorCutoutNoCull(armorItem.getEntityTexture()), false, itemStack.hasEnchantments());
             this.model.render(matrixStack, vertexConsumer, i, OverlayTexture.DEFAULT_UV, q, r, s, 1.0F);
         }
     }
