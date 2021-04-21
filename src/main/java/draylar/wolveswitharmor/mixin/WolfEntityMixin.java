b58fa9c3087ff30d7eebeeaf4eefcd0c05afe4d3
Draylar <samuelhimes@gmail.com>
2021-04-21 16:19:24 -0500

    make wolves drop armor on death (untested because im not killing my test dogs)

--- src/main/java/draylar/wolveswitharmor/mixin/WolfEntityMixin.java
+++ src/main/java/draylar/wolveswitharmor/mixin/WolfEntityMixin.java
@@ -1,13 +1,19 @@
 package draylar.wolveswitharmor.mixin;
 
 import draylar.wolveswitharmor.WolvesWithArmor;
+import draylar.wolveswitharmor.cca.WolfArmorComponent;
 import draylar.wolveswitharmor.item.WolfArmorItem;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.passive.WolfEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ItemScatterer;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WolfEntity.class)
 public abstract class WolfEntityMixin extends TameableEntity {
@@ -34,5 +40,16 @@ public abstract class WolfEntityMixin extends TameableEntity {
         }
 
         return super.isFireImmune();
+    }
+
+    @Inject(
+            method = "onDeath",
+            at = @At("HEAD"))
+    private void dropArmorOnDeath(DamageSource source, CallbackInfo ci) {
+        WolfArmorComponent component = WolvesWithArmor.WOLF_ARMOR.get(this);
+        ItemStack armor = component.getArmor();
+        if(!armor.isEmpty()) {
+            ItemScatterer.spawn(world, getX(), getY(), getZ(), armor);
+        }
     }
 }
